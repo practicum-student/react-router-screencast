d83d037d44f70c9d4626224640c033dd24ef0637
Travis Turner <RoboEnigma@gmail.com>
2020-07-08 22:22:44 +0300

    RR Lesson 1 Task 1 POST

--- emoji-critic/src/components/App.js
+++ emoji-critic/src/components/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 import Dashboard from './Dashboard';
+import Reviews from './Reviews';
+import AboutMe from './AboutMe';
 
 import { Route } from 'react-router-dom';
 
@@ -9,6 +11,8 @@ function App() {
     <div>
       <h1>Emoji Critic</h1>
       <Route path="/" component={Dashboard} />
+      <Route path="/reviews" component={Reviews} />
+      <Route path="/about-me" component={AboutMe} />
     </div>
   );
 }
