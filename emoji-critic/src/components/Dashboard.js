818c08867666a58f5130463de5f1e2cc5b4cff9b
Travis Turner <RoboEnigma@gmail.com>
2020-07-09 11:01:28 +0300

    Removed dynamic user link

--- emoji-critic/src/components/Dashboard.js
+++ emoji-critic/src/components/Dashboard.js
@@ -2,8 +2,6 @@ import React from 'react';
 import './Dashboard.css';
 import { Link } from 'react-router-dom';
 
-const userName = 'xXxemojis_r_lifexXx';
-
 function Dashboard () {
   return (
     <div className="dashboard">
@@ -11,7 +9,7 @@ function Dashboard () {
        The #1 Destination for Emoji Reviews on the World Wide Web since 2020!
       </p>
       <Link to="/reviews">
-        Hello, {userName}! Click here to see my latest reviews!
+        Click here to see my latest reviews!
       </Link>
     </div>
   )
