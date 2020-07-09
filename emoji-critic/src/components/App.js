3c7c8bd6a265bb7b39f07e54c2464a22a461bef4
Travis Turner <RoboEnigma@gmail.com>
2020-07-09 10:31:24 +0300

    RR Lesson 4

--- emoji-critic/src/components/App.js
+++ emoji-critic/src/components/App.js
@@ -4,15 +4,17 @@ import Dashboard from './Dashboard';
 import Reviews from './Reviews';
 import AboutMe from './AboutMe';
 
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div>
       <h1>Emoji Critic</h1>
-      <Route path="/" component={Dashboard} />
-      <Route path="/reviews" component={Reviews} />
-      <Route path="/about-me" component={AboutMe} />
+      <Switch>
+        <Route exact path="/" component={Dashboard} />
+        <Route path="/reviews" component={Reviews} />
+        <Route path="/about-me" component={AboutMe} />
+      </Switch>
     </div>
   );
 }
