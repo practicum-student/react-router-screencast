1c596b3ec58647c14d944db6eb3454eade7fef7b
Travis Turner <RoboEnigma@gmail.com>
2020-07-15 10:07:10 +0300

    RR Nested Routes Task 1 Post

--- emoji-critic/src/components/dashboard/Dashboard.js
+++ emoji-critic/src/components/dashboard/Dashboard.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './Dashboard.css';
-import { Link } from 'react-router-dom';
 
 function Dashboard () {
   return (
@@ -8,9 +7,6 @@ function Dashboard () {
       <p>
        The #1 Destination for Emoji Reviews on the World Wide Web since 2020!
       </p>
-      <Link to="/reviews">
-        Click here to see my latest reviews!
-      </Link>
     </div>
   )
 }
