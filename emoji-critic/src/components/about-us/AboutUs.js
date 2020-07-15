e73de03a4dcd2c024b07b9da7bfa8a97052dcd59
Travis Turner <RoboEnigma@gmail.com>
2020-07-15 20:37:21 +0300

    RR Nested Routes Task 1 Pre (corrected)

---
+++ emoji-critic/src/components/about-us/AboutUs.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './AboutUs.css';
+
+function AboutUs () {
+
+  return (
+    <div>
+      <h2>About us</h2>
+      <p>We'll talk about our mission and our site's history here.</p>
+    </div>
+  )
+}
+
+export default AboutUs;
