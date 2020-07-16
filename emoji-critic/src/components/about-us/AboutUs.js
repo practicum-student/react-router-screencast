7311ff0f82181d6531220463b36c4267bb607fc1
Travis Turner <RoboEnigma@gmail.com>
2020-07-16 11:07:31 +0300

    RR Nested Routes Task 2 Post

--- emoji-critic/src/components/about-us/AboutUs.js
+++ emoji-critic/src/components/about-us/AboutUs.js
@@ -2,12 +2,18 @@ import React from 'react';
 import './AboutUs.css';
 import SiteHistory from './SiteHistory';
 import SiteMission from './SiteMission';
+import { Route } from 'react-router-dom';
 
 function AboutUs () {
   return (
     <div>
       <h2>About us</h2>
-      <p>We'll talk about our mission and our site's history here.</p>
+      <Route>
+        <SiteHistory />
+      </Route>
+      <Route>
+        <SiteMission />
+      </Route>
     </div>
   )
 }
