b80f16929987246229f14e55451509869fb9a68d
Kevin Loughead <kvnloughead@gmail.com>
2021-09-08 11:13:07 -0400

    update styles in header and navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,9 +4,9 @@ import './NavBar.css';
 function NavBar () {
   return (
     <nav className="menu">
-      <a href="" className="menu__link menu__link_active">Home /</a>
-      <a href="" className="menu__link">Reviews /</a>
-      <a href="" className="menu__link">About me</a>
+      <a href="" className="menu__link menu__link_active">Home</a>
+      <a href="" className="menu__link">Emoji Reviews</a>
+      <a href="" className="menu__link">About Me</a>
     </nav>
   )
 }
