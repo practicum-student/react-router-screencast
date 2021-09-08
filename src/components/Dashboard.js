d0490297c8779741ea169992e4e2f326100ef5d2
Kevin Loughead <kvnloughead@gmail.com>
2021-09-08 11:15:02 -0400

    change Home component to Dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-function Home () {
+function Dashboard () {
   return (
-    <div className="home">
+    <div className="dashboard">
       <h2>The #1 Destination on the World Wide Web since 2020!</h2>
       <a href="">The #1 Destination on the World Wide Web since 2020!</a>
     </div>
   )
 }
 
-export default Home;
+export default Dashboard;
