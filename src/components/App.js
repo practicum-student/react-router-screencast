a5aa4635a4d257e04ffd4187c8573c4c418710b1
Kevin Loughead <kvnloughead@gmail.com>
2021-09-08 12:12:44 -0400

    cleanup code

--- src/components/App.js
+++ src/components/App.js
@@ -1,49 +1,17 @@
 import React from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 import './App.css';
 import Header from './Header';
-import Content from './Content'
-import Footer from './Footer';
-
 import AboutMe from './AboutMe';
-import Mission from './Mission';
 import Reviews from './Reviews'
 import Dashboard from './Dashboard'
 
 
 function App() {
   return (
-    // <Router>
-    //   <div className="App">
-    //     <h1>React Router Test</h1>
-    //     <Route path="/aboutme">
-    //       <AboutMe/>
-    //     </Route>
-    //   </div>
-    // </Router>
     <div className="App">
       <Header />
-      {/* IF USE CONTENT WITH NO MODIFIERS */}
-      {/* <Content>
-        <Home />
-      </Content> */}
-
-      {/* IF USE JUSIFIED MODIFIER IT WILL MAKE JUSTIFIED CONTENT INSIDE COLUMN */}
-      {/* <Content mix="justified">
-        <Mission />
-      </Content>
-      <Footer /> */}
-
-      {/* IF USE CENTERED MODIFIER IT WILL MAKE CENTERED CONTENT INSIDE COLUMN */}
-      {/* <Content mix="centered">
-        <AboutMe />
-      </Content> */}
-
-      {/* <Content>
-        <Reviews />
-      </Content>
-      <Footer />  */}
       <Route path="/">
         <Dashboard />
       </Route>
@@ -53,7 +21,6 @@ function App() {
       <Route path="/about-me">
         <AboutMe />
       </Route> 
-
     </div>
   );
 }
--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -5,12 +5,6 @@ import './Reviews.css';
 function Reviews() {
   return (
     <p>My reviews will go here!</p>
-    // <ul className="reviews">
-    //   <li className="reviews__item">👍 Innovation in emoji form</li>
-    //   <li className="reviews__item">👍 Innovation in emoji form</li>
-    //   <li className="reviews__item">👍 Innovation in emoji form</li>
-    //   <li className="reviews__item">👍 Innovation in emoji form</li>
-    // </ul>
   )
 }
 
